2874438a35cb5f5368cba04096a007f57f2d8d0c
Giorgio Garofalo <giogar2705@gmail.com>
2021-05-13 10:22:41 +0200

    Split PiktPropertiesRetriever#retrieve into sub-methods

--- src/main/kotlin/eu/iamgio/pikt/properties/PiktProperties.kt
+++ src/main/kotlin/eu/iamgio/pikt/properties/PiktProperties.kt
@@ -38,6 +38,16 @@ data class PiktProperties(
 class PiktPropertiesRetriever : PropertiesRetriever<PiktProperties> {
 
     /**
+     * Whether an error during the parsing process occurred.
+     */
+    private var isError = false
+
+    private fun error(message: String) {
+        System.err.println(message)
+        isError = true
+    }
+
+    /**
      * Converts raw JVM properties to parsed [PiktProperties].
      * If any required field is missing or invalid it prints an error and exits.
      * @return parsed properties
@@ -51,76 +61,22 @@ class PiktPropertiesRetriever : PropertiesRetriever<PiktProperties> {
         val nativeCompilerPathProperty = System.getProperty("nativecompiler")
         val colorsProperty             = System.getProperty("colors")
 
-        var isError = false
-
-        fun error(message: String) {
-            System.err.println(message)
-            isError = true
-        }
-
         // Source image file
-        val source: File? = if(sourceProperty == null) {
-            error("Source file (-Dsource) is not set.")
-            null
-        } else File(sourceProperty).absoluteFile.also {
-            if(!it.exists()) {
-                error("Source file $it does not exist.")
-            }
-        }
+        val source = source(sourceProperty)
 
         // Output name
-        val output: String = when {
-            outputProperty != null -> outputProperty
-            source != null -> source.nameWithoutExtension
-            else -> "out"
-        }
+        val output = output(outputProperty, source)
 
         // Compilation targets
-        val targets = compTargetsProperty?.split(",")?.map { targetArg ->
-            CompilationTarget.values().firstOrNull { it.argName == targetArg }
-        } ?: emptyList()
-        if(targets.isAnyNull()) {
-            error("One or more compilation target are null.")
-        }
+        val targets = compilationTargets(compTargetsProperty)
 
         // Interpretation target
-        val interpretationTarget = if(interpTargetProperty != null) {
-            CompilationTarget.values().firstOrNull { it.argName == interpTargetProperty }.also {
-                if(it == null) {
-                    error("Invalid intepretation target.")
-                }
-            }
-        } else {
-            null
-        }
+        val interpretationTarget = interpretationTarget(interpTargetProperty)
 
-        // JVM compiler
-        if(jvmCompilerPathProperty == null) {
-            if(targets.isAny(CompilationTarget.JVM)) {
-                error("JVM compiler (-Djvmcompiler) is not set but at least one target is JVM.")
-            }
-        } else if(!File(jvmCompilerPathProperty).exists()) {
-            error("JVM compiler $jvmCompilerPathProperty does not exist.")
-        }
-
-        // Native compiler
-        if(nativeCompilerPathProperty == null) {
-            if(targets.isAnyNative()) {
-                error("Native compiler (-Dnativecompiler) is not set but at least target is native.")
-            }
-        } else if(!File(nativeCompilerPathProperty).exists()) {
-            error("Native compiler $nativeCompilerPathProperty does not exist.")
-        }
+        checkCompilers(jvmCompilerPathProperty, nativeCompilerPathProperty, targets)
 
         // Color scheme
-        val colorsPropertiesRetriever = ColorsPropertiesRetriever()
-        val colorsFile = File("$colorsProperty.properties")
-        if(colorsProperty != null && colorsFile.exists()) {
-            colorsPropertiesRetriever.loadProperties(FileInputStream(colorsFile))
-        } else if(!isError) {
-            println("Color scheme not found. Using the default one.")
-            println("Run Pikt with the -createscheme=name argument to create a scheme.\n")
-        }
+        val colorsPropertiesRetriever = colors(colorsProperty)
 
         if(isError) {
             System.err.println("Could not initialize Pikt properties. Exiting.")
@@ -130,11 +86,80 @@ class PiktPropertiesRetriever : PropertiesRetriever<PiktProperties> {
         return PiktProperties(
                 source = source!!,
                 output = output,
-                compilationTargets = targets.filterNotNull(),
+                compilationTargets = targets,
                 interpretationTarget = interpretationTarget,
                 jvmCompilerPath = jvmCompilerPathProperty,
                 nativeCompilerPath = nativeCompilerPathProperty,
                 colors = colorsPropertiesRetriever.retrieve()
         )
+    }
+
+    private fun source(sourceProperty: String?) =
+            if(sourceProperty == null) {
+                error("Source file (-Dsource) is not set.")
+                null
+            } else File(sourceProperty).absoluteFile.also {
+                if(!it.exists()) {
+                    error("Source file $it does not exist.")
+                }
+            }
+
+    private fun output(outputProperty: String?, source: File?) = when {
+        outputProperty != null -> outputProperty
+        source != null -> source.nameWithoutExtension
+        else -> "out"
+    }
+
+    private fun compilationTargets(compilationTargetsProperty: String?): List<CompilationTarget> {
+        val targets = compilationTargetsProperty?.split(",")?.map { targetArg ->
+            CompilationTarget.values().firstOrNull { it.argName == targetArg }
+        } ?: emptyList()
+        if(targets.isAnyNull()) {
+            error("One or more compilation target are null.")
+        }
+        return targets.filterNotNull()
+    }
+
+    private fun interpretationTarget(interpretationTargetProperty: String?) =
+            if(interpretationTargetProperty != null) {
+                CompilationTarget.values().firstOrNull { it.argName == interpretationTargetProperty }.also {
+                    if(it == null) {
+                        error("Invalid intepretation target.")
+                    }
+                }
+            } else {
+                null
+            }
+
+    private fun colors(colorsProperty: String?): ColorsPropertiesRetriever {
+        val retriever = ColorsPropertiesRetriever()
+        val colorsFile = File("$colorsProperty.properties")
+        if(colorsProperty != null && colorsFile.exists()) {
+            retriever.loadProperties(FileInputStream(colorsFile))
+        } else if(!isError) {
+            println("Color scheme not found. Using the default one.")
+            println("Run Pikt with the -createscheme=name argument to create a scheme.\n")
+        }
+        return retriever
+    }
+
+    private fun checkCompilers(jvmCompilerPath: String?, nativeCompilerPath: String?, targets: List<CompilationTarget>) {
+        // JVM compiler
+        if(jvmCompilerPath == null) {
+            if(targets.isAny(CompilationTarget.JVM)) {
+                error("JVM compiler (-Djvmcompiler) is not set but at least one target is JVM.")
+            }
+        } else if(!File(jvmCompilerPath).exists()) {
+            error("JVM compiler $jvmCompilerPath does not exist.")
+        }
+
+        // Native compiler
+        if(nativeCompilerPath == null) {
+            if(targets.isAnyNative()) {
+                error("Native compiler (-Dnativecompiler) is not set but at least target is native.")
+            }
+        } else if(!File(nativeCompilerPath).exists()) {
+            error("Native compiler $nativeCompilerPath does not exist.")
+        }
     }
 }
