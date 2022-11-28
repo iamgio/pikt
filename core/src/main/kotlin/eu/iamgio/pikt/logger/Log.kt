241264ba6e8b2ba1b61c6419de1f1bec3b214913
Giorgio Garofalo <giogar2705@gmail.com>
2022-11-28 10:35:11 +0100

    Allow setting log level via `-Dloglevel=level`

--- core/src/main/kotlin/eu/iamgio/pikt/logger/Log.kt
+++ core/src/main/kotlin/eu/iamgio/pikt/logger/Log.kt
@@ -23,6 +23,7 @@ object Log {
 
     // Log4J wrapper functions
 
+    fun debug(message: Any) = logger.debug(message)
     fun info(message: Any) = logger.info(message)
     fun warn(message: Any) = logger.warn(message)
     fun error(message: Any) = logger.error(message)
