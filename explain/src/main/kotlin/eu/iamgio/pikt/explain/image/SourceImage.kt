42d241c02c2b59a2275eb1ebb2653a3c615f8f38
Giorgio Garofalo <giogar2705@gmail.com>
2023-02-10 16:55:43 +0100

    Add missing doc

--- explain/src/main/kotlin/eu/iamgio/pikt/explain/image/SourceImage.kt
+++ explain/src/main/kotlin/eu/iamgio/pikt/explain/image/SourceImage.kt
@@ -22,6 +22,12 @@ class SourceImage(val image: BufferedImage) {
     val height: Int
         get() = image.height
 
+    /**
+     * Scales the image by a factor.
+     * @param scaling scale technique to use
+     * @param factor factor to scale the image by
+     * @return a scaled copy of the source image
+     */
     fun scale(scaling: ImageScaling, factor: Int): SourceImage {
         return SourceImage(scaling.scale(this.image, factor))
     }
