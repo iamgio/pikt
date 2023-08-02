152d4b3955e95138d2fe9e6f3c468a2e2a367dde
Giorgio Garofalo <giogar2705@gmail.com>
2023-08-02 16:15:01 +0200

    Add missing docs

--- core/src/main/kotlin/eu/iamgio/pikt/expression/ExpressionTranspiler.kt
+++ core/src/main/kotlin/eu/iamgio/pikt/expression/ExpressionTranspiler.kt
@@ -8,21 +8,50 @@ import eu.iamgio.pikt.image.Pixel
 interface ExpressionTranspiler {
 
     /**
-     *
+     * Transpiles a string [expression] into a string value.
+     * @return the output code
      */
     fun string(expression: StringExpression): String
 
+    /**
+     * Transpiles a string [expression] into a numeric value.
+     * @return the output code
+     */
     fun number(expression: StringExpression): String
 
+    /**
+     * Transpiles a boolean [expression].
+     * @return the output code
+     */
     fun boolean(expression: BooleanExpression): String
 
+    /**
+     * Transpiles a function call [expression].
+     * @return the output code
+     */
     fun functionCall(expression: FunctionCallExpression): String
 
+    /**
+     * Transpiles a struct initialization [expression].
+     * @return the output code
+     */
     fun structInit(expression: StructInitExpression): String
 
+    /**
+     * Transpiles an [operator].
+     * @return the output code
+     */
     fun operator(operator: Operator): String
 
+    /**
+     * Transpiles a single [symbol].
+     * @return the output code
+     */
     fun symbol(symbol: Pixel): String
 
+    /**
+     * Transpiles a [sequence] of nested symbols.
+     * @return the output code
+     */
     fun sequence(sequence: PixelSequence): String
 }
